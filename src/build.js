7e6b1a1112e2a1403923819a914d2c25a389261e
Michael Spencer <sonrisesoftware@gmail.com>
2017-04-15 18:03:27 -0500

    Use the patched version of babel after typescript

--- src/build.js
+++ src/build.js
@@ -7,6 +7,21 @@
   const gresources_lookup_data = imports.gi.Gio.resources_lookup_data
   const GResourceLookupFlags = imports.gi.Gio.ResourceLookupFlags
 
+  const Babel = imports.jsgtk.babel.Babel
+  const BabelTransformer = {plugins: [
+    'transform-decorators-legacy',
+    'transform-class-properties',
+    'transform-flow-strip-types',
+    'transform-es2015-classes',
+    'transform-es2015-literals',
+    'transform-es2015-object-super',
+    'transform-es2015-parameters',
+    'transform-es2015-shorthand-properties',
+    'transform-es2015-unicode-regex',
+    'transform-exponentiation-operator',
+    'transform-es2015-template-literals',
+  ]}
+
   function findPackage(dir) {
     const filename = path.join(dir, 'package.json')
 
@@ -15,6 +30,13 @@
     } else {
       return findPackage(path.dirname(dir))
     }
+  }
+
+  function transpile(path) {
+    const code = fs.readFileSync(path, 'utf8')
+    const transpiled = Babel.transform(code, BabelTransformer).code
+
+    fs.writeFileSync(path, transpiled)
   }
 
   exports.build = function(appId, mainFile, resourceFile, depsFile) {
@@ -33,7 +55,8 @@
 
     fs.writeFileSync(path.join(outDir, 'main.js'), main)
 
-    child_process.execSync('tsc ' + sourceDir +  '/index.ts' + ' --alwaysStrict --outDir ' + outDir)
+    child_process.execSync('tsc ' + sourceDir +  '/index.ts' + ' --alwaysStrict --outDir ' + outDir +
+                           ' -t ES2015 --module commonjs')
 
     const sourceFiles = fs.readdirSync(sourceDir)
       .filter(file => file.endsWith('.ts'))
@@ -42,6 +65,10 @@
       .filter(file => file.endsWith('.ts'))
       .map(file => file.replace('.ts', '.js'))
       .concat(['main.js'])
+
+    for (let file of files) {
+      transpile(path.join(outDir, file))
+    }
 
     const resources = '<?xml version="1.0" encoding="UTF-8"?>\n' +
                       '<gresources>\n' +
